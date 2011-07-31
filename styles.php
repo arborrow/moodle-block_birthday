939782a39cbae5bab82a6aeefcb7c2c6e59f566a
Anthony Borrow <anthony@moodle.org>
2011-07-30 22:46:30 -0500

    CONTRIB-1989 - removing message formatting from styles.php

--- styles.php
+++ styles.php
@@ -1,9 +1,7 @@
-.block_birthday .listentry div.user,
-.block_messages .listentry div.user {
+.block_birthday .listentry div.user {
   float:left;
 }
 
-.block_birthday .listentry div.message,
-.block_messages .listentry div.message {
+.block_birthday .listentry div.message {
   float:right;
 }
